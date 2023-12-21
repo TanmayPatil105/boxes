68ca7fd1d607089cfdd8fef84a8a1cfd1a250650
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-12-21 17:55:26 +0530

    test each padding level

--- main.cpp
+++ main.cpp
@@ -6,9 +6,50 @@
 
 #include "Box.h"
 
-int main() {
-  Box box = Box(3, 7, PADDING_LEVEL_LARGE);
+void
+test_padding_none (void)
+{
+  std::cout << "NONE:" << std::endl;
+
+  Box box = Box(3, 3, PADDING_LEVEL_NONE);
 
   box.draw();
+}
+
+void
+test_padding_small (void)
+{
+  std::cout << "SMALL:" << std::endl;
+
+  Box box = Box(3, 3, PADDING_LEVEL_SMALL);
+
+  box.draw();
+}
+
+void
+test_padding_normal (void)
+{
+  std::cout << "NORMAL:" << std::endl;
+
+  Box box = Box(3, 3, PADDING_LEVEL_NORMAL);
+
+  box.draw();
+}
+
+void
+test_padding_large (void)
+{
+  std::cout << "LARGE:" << std::endl;
+
+  Box box = Box(3, 3, PADDING_LEVEL_LARGE);
+
+  box.draw();
+}
+
+int main() {
+  test_padding_none ();
+  test_padding_small ();
+  test_padding_normal ();
+  test_padding_large ();
   return 0;
 }
